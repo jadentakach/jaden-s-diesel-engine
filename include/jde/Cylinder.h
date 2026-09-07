8a6e4af0684d915fbead38647cbed7a43817d4d8
jadentakach <jadentakach@gmail.com>
2026-09-07 15:14:13 -0400

    ctors

--- include/jde/Cylinder.h
+++ include/jde/Cylinder.h
@@ -2,5 +2,21 @@
 #include <jde/Piston.h>
 
 class Cylinder {
-    
+private:
+    Piston* piston_;
+    float bore_;
+    float stroke_;
+
+public:
+    Cylinder() {
+        piston_ = new Piston;
+    }
+
+    Cylinder(float bore, float stroke) {
+        bore_ = bore;
+        stroke_ = stroke;
+        
+        piston_ = new Piston;
+        piston_->SetStroke(stroke_);
+    }
 };
